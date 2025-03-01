bb10477b8654afa376ce3fb2ddbee88a3dc47636
raphaellith <raphaellith@gmail.com>
2025-03-01 18:32:16 +0000

    Rewrote scraper.py with OOP

--- groupingarticles.py
+++ groupingarticles.py
@@ -1,51 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import re
 
-def get_titles_from_the_guardian(get_link=False):
-    url = "https://www.theguardian.com/technology/all"
+from scraper import BBCScraper, TheGuardianScraper
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    result = []
-
-    for a_tag in soup.find_all("a"):
-        data_link_name = a_tag.get('data-link-name')
-        if data_link_name is not None:
-            if data_link_name.startswith("news") or data_link_name.startswith("feature"):
-                title = a_tag.get('aria-label')
-                if get_link:
-                    link = "https://www.theguardian.com" + a_tag.get('href')
-                    result.append((title, link))
-                else:
-                    result.append(title)
-    return result
-
-def get_titles_from_bbc(get_link=False):
-    url = "https://www.bbc.co.uk/news/technology"
-
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    result = []
-
-    for a_tag in soup.find_all("a"):
-        if a_tag.get('href') is not None:
-            link = a_tag.get('href')
-            if link.startswith('/news'):
-                children = a_tag.find_all("span")
-                for child in children:
-                    if child.get('aria-hidden') == 'false':
-                        title = child.text
-                        if get_link:
-                            link = 'https://www.bbc.co.uk' + link
-                            result.append((title, link))
-                        else:
-                            result.append(title)
-    return result
 
 def clean_and_tokenize(headlines):
     common_words = {'a', 'an', 'the', 'and', 'or', 'but', 'if', 'then', 'else', 'on', 'in', 'at', 'of', 'for', 'to', 'with'}
@@ -56,6 +14,7 @@ def clean_and_tokenize(headlines):
         if cleaned_words:  # ensure headline is not empty after cleaning
             cleaned_headlines.append(' '.join(cleaned_words))
     return cleaned_headlines
+
 
 # compare headlines using cosine similarity
 def compare_headlines(headlines1, headlines2, threshold=0.2):
@@ -79,9 +38,10 @@ def compare_headlines(headlines1, headlines2, threshold=0.2):
     
     return matches
 
+
 def main():
-    guardian_headlines = get_titles_from_the_guardian()
-    bbc_headlines = get_titles_from_bbc()
+    guardian_headlines = TheGuardianScraper().get_titles(topic="technology")
+    bbc_headlines = BBCScraper().get_titles(topic="technology")
 
     print("Guardian Headlines:", guardian_headlines)
     print("BBC Headlines:", bbc_headlines)
@@ -101,6 +61,7 @@ def main():
         print(f"Guardian: {match[0]}")
         print(f"BBC: {match[1]}")
         print(f"Similarity Score: {match[2]:.2f}\n")
+
 
 if __name__ == "__main__":
     main()
--- scraper.py
+++ scraper.py
@@ -2,50 +2,80 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_titles_from_the_guardian(get_link=True):
-    url = "https://www.theguardian.com/technology/all"
+# Currently we only support these three topics, but more can be added in the future
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+SUPPORTED_TOPICS = [
+    "technology",
+    "business",
+    "culture"
+]
 
-    result = []
 
-    for a_tag in soup.find_all("a"):
-        data_link_name = a_tag.get('data-link-name')
-        if data_link_name is not None:
-            if data_link_name.startswith("news") or data_link_name.startswith("feature"):
-                title = a_tag.get('aria-label')
-                if get_link:
+class Scraper:
+    def __init__(self):
+        pass
+
+    def get_title_link_pairs(self, topic: str) -> [(str, str)]:  # ABSTRACT
+        """
+        Given a topic, returns a list of (title, link) pairs scraped from the site.
+        The topic is a string that must be one of the SUPPORTED_TOPICS. See above.
+        """
+        raise NotImplementedError("get_title_link_pairs() is abstract and should be implemented!")
+
+    def get_titles(self, topic: str) -> [str]:
+        """
+        Given a topic, returns a list of titles scraped from the site.
+        """
+        return list(list(zip(*self.get_title_link_pairs(topic=topic)))[0])
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class TheGuardianScraper(Scraper):
+    def __init__(self):
+        super().__init__()
+
+    def get_title_link_pairs(self, topic) -> [(str, str)]:
+        assert topic in SUPPORTED_TOPICS
+        url = f"https://www.theguardian.com/{topic}/all"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        result = []
+
+        for a_tag in soup.find_all("a"):
+            data_link_name = a_tag.get('data-link-name')
+            if data_link_name is not None:
+                if data_link_name.startswith("news") or data_link_name.startswith("feature"):
+                    title = a_tag.get('aria-label')
                     link = "https://www.theguardian.com" + a_tag.get('href')
                     result.append((title, link))
-                else:
-                    result.append(title)
 
-    return result
+        return result
 
 
-def get_titles_from_bbc(get_link=True):
-    url = "https://www.bbc.co.uk/news/technology"
+class BBCScraper(Scraper):
+    def __init__(self):
+        super().__init__()
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+    def get_title_link_pairs(self, topic) -> [(str, str)]:
+        assert topic in SUPPORTED_TOPICS
+        url = f"https://www.bbc.co.uk/news/{topic}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    result = []
+        result = []
 
-    for a_tag in soup.find_all("a"):
-        if a_tag.get('href') is not None:
-            link = a_tag.get('href')
-            if link.startswith('/news'):
-                children = a_tag.find_all("span")
-                for child in children:
-                    if child.get('aria-hidden') == 'false':
-                        title = child.text
-                        if get_link:
+        for a_tag in soup.find_all("a"):
+            if a_tag.get('href') is not None:
+                link = a_tag.get('href')
+                if link.startswith('/news'):
+                    children = a_tag.find_all("span")
+                    for child in children:
+                        if child.get('aria-hidden') == 'false':
+                            title = child.text
                             link = 'https://www.bbc.co.uk' + link
                             result.append((title, link))
-                        else:
-                            result.append(title)
 
-    return result
-
-
+        return result
